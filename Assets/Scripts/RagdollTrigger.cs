28e6d09a00d08dcfcdc1d657616976e74972bc22
Jack Aylward <jack.t.aylward@gmail.com>
2023-02-22 12:04:49 +1100

    unity proj

---
+++ Assets/Scripts/RagdollTrigger.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RagdollTrigger : MonoBehaviour
+{
+	private void OnTriggerEnter(Collider other)
+	{
+		Ragdoll ragdoll = other.GetComponentInParent<Ragdoll>();
+		if(ragdoll != null)
+			ragdoll.ragdollOn = true;
+	}
+	
+}
